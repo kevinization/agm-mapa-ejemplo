7a220809a381091b7bda33495864bc15c8b87ed4
kevinization <kevinrodarte23@gmail.com>
2020-11-16 21:43:17 -0600

    Ya obtiene latitud y longitud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MouseEvent } from '@agm/core';
 
 @Component({
   selector: 'app-root',
@@ -6,23 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // Cosas que suelen tener los mapas:
-  lat: number;
-  lng: number;
-  zoom: number;
-  mapTypeId: string;
-  // Para que no salga el marcador al principio
-  located: boolean;
 
-  constructor(){
-    this.lat = 24.0277;
-    this.lng = -104.653;
-    this.zoom = 6;
-    this.mapTypeId = 'hybrid';
-    // located se pone en false al principio para que no salga al cargar la página
-    this.located = false;
 
+lat = 24.0277;
+lng = -104.653;
+zoom = 6;
+mapTypeId = 'roadmap';
+located = false;
+
+  markers: marker[] = [];
+
+  // tslint:disable-next-line: typedef
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`);
   }
+
+  // tslint:disable-next-line: typedef
+  mapClicked($event: MouseEvent) {
+    this.markers.push({
+      lat: $event.coords.lat,
+      lng: $event.coords.lng,
+      draggable: true
+    });
+  }
+
+  // tslint:disable-next-line: typedef
+  markerDragEnd(m: marker, $event: MouseEvent) {
+    console.log('dragEnd', m, $event);
+    console.log('la latitud es: ' + $event.coords.lat);
+    console.log('la longitud es: ' + $event.coords.lng);
+
+    this.lat = $event.coords.lat;
+    this.lng = $event.coords.lng;
+  }
+
 
   // tslint:disable-next-line: typedef
   obtenerPosicionActual(){
@@ -35,4 +53,17 @@ export class AppComponent {
       this.located = true;
     });
   }
+    // tslint:disable-next-line: typedef
+    coordsFinales(){
+      console.log('LATITUD FINAL: ' + this.lat);
+      console.log('LONGITUD FINAL: ' + this.lng);
+    }
 }
+// tslint:disable-next-line: class-name
+interface marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
+}
+
